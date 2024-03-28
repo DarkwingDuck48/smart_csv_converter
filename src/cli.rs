a74ed399c1774b219bbd07ef8c58f3aa0d1078bf
Maxim Britvin <maksbritvin@gmail.com>
2024-03-28 20:27:35 +0300

    refactor cli arguments to different groups with relations. Config arguments can't used with manual sheets

--- src/cli.rs
+++ src/cli.rs
@@ -1,9 +1,31 @@
+use clap::ArgGroup;
 use clap::Parser;
 use std::path::PathBuf;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
+#[clap(group(
+ArgGroup::new("settings")
+.required(true)
+.args(& ["parsed_sheets", "named_ranges"])
+))]
+#[clap(group(ArgGroup::new("manual").multiple(true).args(& ["source_file", "target_file"])))]
 pub struct Cli {
+    /// Path to source file (manual import)
+    #[arg(long, requires = "target_file")]
+    pub source_file: Option<PathBuf>,
+
+    /// Path to target file (manual import)
+    #[arg(long, requires = "source_file")]
+    pub target_file: Option<PathBuf>,
+
+    // Ranges with defined names to parse on sheets
+    #[arg(short = 'r', long = "ranges", value_delimiter = ' ', num_args = 1..)]
+    pub named_ranges: Vec<String>,
+
+    /// Parsed sheets in file (required source and target sheets)
+    #[arg(short = 's', long = "sheets", value_delimiter = ' ', num_args = 1..)]
+    pub parsed_sheets: Vec<String>,
 
     /// Debug Mode
     #[arg(short, long, action, default_value = "false")]
@@ -13,24 +35,8 @@ pub struct Cli {
     #[arg(short = 'l', long = "log")]
     pub log_file: Option<PathBuf>,
 
-    /// Path to source file (manual import)
-    #[arg(long, group = "manual")]
-    pub source_file: Option<PathBuf>,
-
-    /// Path to target file (manual import)
-    #[arg(long, group = "manual")]
-    pub target_file: Option<PathBuf>,
-
-    /// Parsed sheets in file (required source and target sheets)
-    #[arg(short = 's', long = "sheets", value_delimiter = ' ', requires = "manual", num_args = 1..)]
-    pub parsed_sheets: Vec<String>,
-
-    // Ranges with defined names to parse on sheets
-    #[arg(short = 'r', long = "ranges", value_delimiter = ' ', requires = "manual", num_args = 1..)]
-    pub named_ranges: Vec<String>,
-
     /// Path to config TOML file (only config file needed)
-    #[arg(long = "config")]
+    #[arg(long = "config", conflicts_with = "manual")]
     pub config: Option<PathBuf>,
 }
 
